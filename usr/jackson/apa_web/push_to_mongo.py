c57799b815b6e1228ffb53822a8043bd64983447
Jackson Cummings <barbercummings@gmail.com>
2019-09-21 12:12:09 -0500

    finished pushing data to mongo

---
+++ usr/jackson/apa_web/push_to_mongo.py
@@ -0,0 +1,32 @@
+# import necessary libraries
+from flask_pymongo import PyMongo
+import csv
+from flask import Flask
+
+# Use PyMongo to establish Mongo connection
+app = Flask(__name__)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/pets_db")
+
+# Pushing aac data into mongo
+csvfile = open('../csv_docs/austin_animal_center_outcomes.csv', 'r')
+reader = csv.DictReader( csvfile )
+mongo.db.animals.drop()
+header= ["Animal ID", "Name", "DateTime", "MonthYear", "Date of Birth", "Outcome Type", "Outcome Subtype", "Animal Type", "Sex upon Outcome", "Age upon Outcome", "Breed", "Color"]
+for each in reader:
+    row={}
+    for field in header:
+        row[field]=each[field]
+
+    mongo.db.animals.insert_one(row)
+
+# Pushing shelter coordinates data into mongo
+csvfile_2 = open('../csv_docs/shelters_coordinates_austin.csv', 'r')
+reader = csv.DictReader( csvfile_2 )
+mongo.db.coordinates.drop()
+header= ["Animal ID", "Lat", "Lng"]
+for each in reader:
+    row={}
+    for field in header:
+        row[field]=each[field]
+
+    mongo.db.coordinates.insert_one(row)
