d87c4cd6e65b591198a58263839f8d95ef7f6f93
jpastell <castellanos.juan.pablo@gmail.com>
2019-09-23 23:49:53 -0500

    Update apa.js
    Adding sun-burst plot

--- release/static/js/apa.js
+++ release/static/js/apa.js
@@ -3,7 +3,6 @@
 //---------------------------------------------
 const apa_url = "http://127.0.0.1:5000/apa_api";
 
-// var colorScale = ["#e76818","#f29e2e", "#ffff8c","#90eb9d","#00a6ca"];
 var colorScale = ["#FF5733","#FFC300", "#DAF7A6","#7DCEA0 ","#58D68D"];
 
 var margin = {
@@ -16,7 +15,165 @@ width = 700,
 height = 350;
 //---------------------------------------------
 
-//When call draw the spider chard on the div with the ID
+//****************************************************************************
+//*                         Sun Burst Drawing                                *
+//****************************************************************************
+//Function used for ploting the Sun-Burst data
+function plotSunBurst(apaData){
+  var data = [{
+    type: "sunburst",
+    labels: [ "Dogs",
+              "Female",
+              "Male",
+              "Foster",
+              "TLAC",
+              "Foster",
+              "TLAC",
+              "Puppy",
+              "Puppy",
+              "Puppy",
+              "Puppy"],
+    ids:    [ "Dogs",
+              "Female",
+              "Male",
+              "Female - Foster",
+              "Female - TLAC",
+              "Male - Foster",
+              "Male - TLAC",
+              "Male - Foster - Puppy",
+              "Male - TLAC - Puppy",
+              "Female - Foster - Puppy",
+              "Female - TLAC - Puppy"],
+    parents:[ "",
+              "Dogs",
+              "Dogs",
+              "Female",
+              "Female",
+              "Male",
+              "Male",
+              "Male - Foster",
+              "Male - TLAC",
+              "Female - Foster",
+              "Female - TLAC"],
+    values: [ apaData.dogs.total,
+              apaData.dogs.female.total,
+              apaData.dogs.male.total,
+              apaData.dogs.female.foster.total,
+              apaData.dogs.female.tlac.total,
+              apaData.dogs.male.foster.total,
+              apaData.dogs.male.tlac.total,
+              apaData.dogs.male.foster.puppy.total,
+              apaData.dogs.male.tlac.puppy.total,
+              apaData.dogs.female.foster.puppy.total,
+              apaData.dogs.female.tlac.puppy.total],
+    outsidetextfont: {size: 20, color: "#377eb8"},
+    insidetextfont: {size: 15, color: "#FDFEFE"},
+    leaf: {opacity: .9},
+    marker: {line: {width: 2}, colors:[ '#FDFEFE',
+                                        '#D7BDE2',
+                                        '#85C1E9',
+                                        '#A2D9CE',
+                                        '#F5B7B1',
+                                        '#A2D9CE',
+                                        '#F5B7B1',
+                                        '#A9DFBF',
+                                        '#E6B0AA',
+                                        '#A9DFBF',
+                                        '#E6B0AA']},
+    branchvalues: 'total'
+  }];
+
+  var layout = {
+    margin: {l: 0, r: 0, b: 0, t: 0},
+    width: 500,
+    height: 500
+  };
+  Plotly.newPlot('sunBurst', data, layout);
+}
+
+//Function used to init the data for drawing
+function newSunBurstData(){
+  sunBurstData =
+  {"dogs":
+    {
+      "total":0,
+      "female":{
+              "total":0,
+              "foster":{
+                      "total":0,
+                      "puppy":{"total":0}
+                      },
+              "tlac":{
+                      "total":0,
+                      "puppy":{"total":0}
+                     },
+              },
+      "male": {
+              "total":0,
+              "foster":{
+                      "total":0,
+                      "puppy":{"total":0}
+                      },
+              "tlac":{
+                      "total":0,
+                      "puppy":{"total":0}
+                     },
+              }
+    }
+  };
+  return sunBurstData;
+}
+
+//Function used to update the sun burst data
+function updateSunBurst(apaData,entryData){
+  //Update the total value
+  apaData.dogs.total+=1;
+  //Sex categorization
+  if(entryData.pet_sex === "Male"){
+    //Update Male data
+    apaData.dogs.male.total+=1;
+    var dogPtr = apaData.dogs.male;
+  } else {
+    //Update Female data
+    apaData.dogs.female.total+=1;
+    var dogPtr = apaData.dogs.female;
+  }
+  //Update foster vs TLAC
+  if(entryData.pet_location === "TLAC"){
+    //TLAC update
+    dogPtr.tlac.total+=1;
+    var varPuppyPtr = dogPtr.tlac.puppy;
+  } else {
+    //Foster update
+    dogPtr.foster.total+=1;
+    var varPuppyPtr = dogPtr.foster.puppy;
+  }
+  //Uodate puppy
+  if(entryData.pet_age  <= 0.5 ){
+    varPuppyPtr.total+=1;
+  }
+}
+
+//function used to log the sunburst data to be ploted
+function logSunBurstData(apaData) {
+  console.log("Dogs: %d",apaData.dogs.total);
+  console.log("|\tFemale: %d",apaData.dogs.female.total);
+  console.log("|\t|\tFoster: %d",apaData.dogs.female.foster.total);
+  console.log("|\t|\t|\tPuppy: %d",apaData.dogs.female.foster.puppy.total);
+  console.log("|\t|\tTLAC: %d",apaData.dogs.female.tlac.total);
+  console.log("|\t|\t|\tPuppy: %d",apaData.dogs.female.tlac.puppy.total);
+
+  console.log("|\tMale: %d",apaData.dogs.male.total);
+  console.log("|\t|\tFoster: %d",apaData.dogs.male.foster.total);
+  console.log("|\t|\t|\tPuppy: %d",apaData.dogs.male.foster.puppy.total);
+  console.log("|\t|\tTLAC: %d",apaData.dogs.male.tlac.total);
+  console.log("|\t|\t|\tPuppy: %d",apaData.dogs.male.tlac.puppy.total);
+}
+
+//****************************************************************************
+//*                             Spyder Chart                                 *
+//****************************************************************************
+//Function used to build the spider chart
 function buildSpider(dataObj){
   	Plotly.purge("spyderDraw");
     data = [{
@@ -24,7 +181,7 @@ function buildSpider(dataObj){
         r: [dataObj.stats.cat, dataObj.stats.child, dataObj.stats.dog, dataObj.stats.homealone, dataObj.stats.cat],
         theta: ['Cat','Child','Dog', 'Home Alone', "Cat"],
         fill: 'toself',
-        name: "How good it gets with"
+        name: "Gets along with"
       }]
 
       var sheHe = "he is in";
@@ -41,9 +198,6 @@ function buildSpider(dataObj){
                   dataObj.age.years +"Y "+ dataObj.age.month + "M ) " +
                   sheHe;
       }
-
-      // "age":{"years":years,"month":month},
-
       layout = {
         title:newName,
         polar: {
@@ -54,14 +208,13 @@ function buildSpider(dataObj){
         },
         showlegend: true
       }
-
-      // Plotly.plot("tipDiv", data, layout)
       Plotly.plot("spyderDraw", data, layout);
 };
+//****************************************************************************
 
 //Fuction used to calculate the color base on the average stats and the age
 function get_color(data){
-  if(data.pet_age < 0.5){
+  if(data.pet_age <= 0.5){
     //If dog is less than 6 months more likelly to get
     //adopted
     return colorScale[colorScale.length-1];
@@ -99,17 +252,18 @@ function process_apa_data(factData){
   var MapRows = i;
   var MapColumns = (i + Math.round(i/2));
 
-  console.log("Data length: %d",factData.length);
-  console.log("y:%d",MapRows);
-  console.log("x:%d",MapColumns);
-  console.log(factData);
-
   //We found the porper size to create a rectangle for the amount of data now
   //lets calculate the color by averaging the the stats
 
+  //[*] Used to store hxagon data
   var petData = [];
+  //[*] SunBurst adata stored
+  sunBurstData = newSunBurstData();
 
+  //This is the loop used to iterate the data, this is used to collet all info
+  //to avoid multiple iterations of the data
   factData.forEach(function(data) {
+    //Data collection for hex data
     var years = Math.floor(data.pet_age);
     var month = Math.round((data.pet_age - years) * 12);
     petData.push({"hexColor":get_color(data),
@@ -119,8 +273,19 @@ function process_apa_data(factData){
                   "location":data.pet_location,
                   "stats":{"cat":data.cat,"dog":data.dog,"child":data.child,"homealone":data.homealone}
                 });
+
+    //Data collection for plotting the sunburt
+    updateSunBurst(sunBurstData,data);
   });
 
+  //Log data for debug
+  logSunBurstData(sunBurstData);
+  //Plot the sun burst data
+  plotSunBurst(sunBurstData);
+
+  //****************************************************************************
+  //*                      Hexagon Ploting Drawing                             *
+  //****************************************************************************
   //Pad the data for missing data to complete the square
   for (let i = 0; i < ((MapRows * MapColumns) - factData.length); ++i) {
     petData.push({"hexColor":"white",
@@ -150,15 +315,17 @@ function updateToolTip(hexPathGroup) {
   //On click
   hexPathGroup.on("click", function(data) {
     toolTip.show();
-    console.log(data);
     buildSpider(data.data);
   }).on("mouseout", function(data, index) {
       toolTip.hide(data);
+  }).on("mouseover", function(d) {
+    //ToDo: Add lighter color here
+    console.log(data);
   });
   return hexPathGroup;
 }
 
-//Fuction usid to initilize data at the begining
+//Fuction used to initilize data at the begining
 function init(){
   buildSpider({"name":"Click in any hexagon to see the stats",
               "stats":{"cat":0,"child":0,"dog":0,"homealone":0}});
@@ -172,7 +339,6 @@ d3.json(apa_url).then(function(data) {
 
   //Process the data
   var plotObj = process_apa_data(data);
-  console.log(plotObj);
 
   //The maximum radius the hexagons can have to still fit the screen
   var hexRadius = d3.min([width/((plotObj.honneyComb.x + 0.5) * Math.sqrt(3)),
@@ -196,24 +362,18 @@ d3.json(apa_url).then(function(data) {
   //Set the hexagon radius
   var hexbin = d3.hexbin().radius(hexRadius);
 
-
-  console.log(plotObj.petData[0]["hexColor"]);
-
   //console.log(hexbin(points));
   var cart = [];
   hexBinPoints = hexbin(points);
 
-  console.log(hexBinPoints.length);
-  console.log(plotObj.petData.length);
-
+  //binding the data for the hexagon
   var hexIndex = 0;
   hexBinPoints.forEach(function (x) {
     cart.push({"hexObj":x,"data":plotObj.petData[hexIndex]});
     hexIndex += 1;
   });
 
-  console.log(cart);
-
+  //Here is where the drawing happens
   //Draw the hexagons
   var hexCount = svg.append("g");
 
@@ -234,5 +394,7 @@ d3.json(apa_url).then(function(data) {
                       }
       );
 
+  //Tooltip update
   updateToolTip(hexaPath);
+  //****************************************************************************
 });
