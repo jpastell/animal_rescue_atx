965f73aba4f2c64a0cb83d0c9b2fa07bcf342809
Jackson Cummings <barbercummings@gmail.com>
2019-09-24 21:14:51 -0500

    removed shelter mongo push

--- release/push_to_mongo.py
+++ release/push_to_mongo.py
@@ -7,17 +7,17 @@ from flask import Flask
 app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/pets_db")
 
-# Pushing aac data into mongo
-csvfile = open('../csv_docs/austin_animal_center_outcomes.csv', 'r')
-reader = csv.DictReader( csvfile )
-mongo.db.animals.drop()
-header= ["Animal ID", "Name", "DateTime", "MonthYear", "Date of Birth", "Outcome Type", "Outcome Subtype", "Animal Type", "Sex upon Outcome", "Age upon Outcome", "Breed", "Color"]
-for each in reader:
-    row={}
-    for field in header:
-        row[field]=each[field]
+# # Pushing aac data into mongo
+# csvfile = open('../csv_docs/austin_animal_center_outcomes.csv', 'r')
+# reader = csv.DictReader( csvfile )
+# mongo.db.animals.drop()
+# header= ["Animal ID", "Name", "DateTime", "MonthYear", "Date of Birth", "Outcome Type", "Outcome Subtype", "Animal Type", "Sex upon Outcome", "Age upon Outcome", "Breed", "Color"]
+# for each in reader:
+#     row={}
+#     for field in header:
+#         row[field]=each[field]
 
-    mongo.db.animals.insert_one(row)
+#     mongo.db.animals.insert_one(row)
 
 # Pushing shelter coordinates data into mongo
 csvfile_2 = open('../csv_docs/shelters_coordinates_austin_updated.csv', 'r')
